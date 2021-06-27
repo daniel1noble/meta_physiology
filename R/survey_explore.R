d1f37765ae81493027294cd348a04e67ac945449
Daniel Noble <daniel.wa.noble@gmail.com>
2021-06-27 16:34:45 +1000

    Some code

--- R/survey_explore.R
+++ R/survey_explore.R
@@ -8,3 +8,15 @@
 	data <- read.csv("./data/MA_CompPysio_Survey.csv")
 # Have a look
 	data %>% glimpse()
+
+# Lets just subset to the main questions for simplicity
+
+	data2 <- data %>% select(!grep("Comment", colnames(data)))
+
+# Just some simple plots. Ok, needs some cleaning looking at this. Studies often used multiple effects
+
+	ggplot(data2, aes(x = Q2.1_EffectUsed)) + 
+		geom_histogram(stat = "count") +
+		theme(axis.text = element_text(size =6))
+
+
